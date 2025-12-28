7502cd6ac0ddd0c388256c6e4aa363cd01cb084a
Nicolas Thery <nthery@gmail.com>
2025-12-28 10:04:18 +0100

    gift_shop: implement parser

--- src/bin/gift_shop/main.rs
+++ src/bin/gift_shop/main.rs
@@ -1,5 +1,5 @@
 ///! Day 2 - The Gift Shop
-use std::io::BufRead;
+use std::io::{BufRead, Split};
 
 fn main() -> anyhow::Result<()> {
     let input = advent_code_25::open_input_file()?;
@@ -9,39 +9,82 @@ fn main() -> anyhow::Result<()> {
 }
 
 fn solve_for<R: BufRead>(input: R) -> anyhow::Result<usize> {
-    let mut nb_invalid_id = 0;
-    for range in ranges_from_input(input) {
-        nb_invalid_id += sum_invalid_ids_in_range(range?);
+    let mut acc = 0;
+    for range in ranges_from_input(input)? {
+        acc += sum_invalid_ids_in_range(range?);
     }
-    Ok(nb_invalid_id)
+    Ok(acc)
 }
 
-fn ranges_from_input<R: BufRead>(input: R) -> impl Iterator<Item = anyhow::Result<Range>> {
-    RangeIter
+/// Returns iterator that parses `input` into a sequence of ID ranges.
+fn ranges_from_input<R: BufRead>(
+    input: R,
+) -> anyhow::Result<impl Iterator<Item = anyhow::Result<Range>>> {
+    Ok(RangeIter::new(input.split(b',')))
 }
 
 /// Closed range spanning from .0 to .1 inclusive.
-#[derive(Debug, Copy, Clone)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
 struct Range(usize, usize);
 
-struct RangeIter;
+/// Iterator that yields Range values.
+struct RangeIter<R: BufRead> {
+    ranges: Split<R>,
+}
 
-impl Iterator for RangeIter {
-    type Item = anyhow::Result<Range>;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        todo!()
+impl<R: BufRead> RangeIter<R> {
+    fn new(ranges: Split<R>) -> Self {
+        Self { ranges }
     }
 }
 
-fn sum_invalid_ids_in_range(range: Range) -> usize {
-    let mut nb_invalid_id = 0;
-    for id in range.0..=range.1 {
-        if is_invalid_id(id) {
-            nb_invalid_id += 1
+impl<R: BufRead> Iterator for RangeIter<R> {
+    type Item = anyhow::Result<Range>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.ranges.next() {
+            Some(range) => match range {
+                Ok(range) => Some(parse_id_range(&range)),
+                Err(e) => Some(Err(anyhow::anyhow!("failed to parse ID range: {}", e))),
+            },
+            None => None,
         }
     }
-    nb_invalid_id
+}
+
+fn parse_id_range(input: &[u8]) -> anyhow::Result<Range> {
+    if let Some(pos) = input.iter().position(|&c| c == b'-') {
+        Ok(Range(
+            parse_id(&input[..pos])?,
+            parse_id(&input[pos + 1..])?,
+        ))
+    } else {
+        anyhow::bail!("missing '-' in ID range: {:?}", input);
+    }
+}
+
+fn parse_id(input: &[u8]) -> anyhow::Result<usize> {
+    if input.is_empty() {
+        anyhow::bail!("empty ID");
+    }
+    let mut n = 0_usize;
+    for &b in input {
+        if !b.is_ascii_digit() {
+            anyhow::bail!("unexpected character in ID: {}", b)
+        }
+        n = n * 10 + (b - b'0') as usize;
+    }
+    Ok(n)
+}
+
+fn sum_invalid_ids_in_range(range: Range) -> usize {
+    let mut acc = 0;
+    for id in range.0..=range.1 {
+        if is_invalid_id(id) {
+            acc += id
+        }
+    }
+    acc
 }
 
 fn is_invalid_id(id: usize) -> bool {
@@ -64,6 +107,36 @@ fn nb_digits(n: usize) -> u32 {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::io::Cursor;
+
+    #[test]
+    fn test_parse_id() {
+        assert!(matches!(parse_id(b"1"), Ok(1)));
+        assert!(matches!(parse_id(b"123"), Ok(123)));
+        assert!(matches!(parse_id(b"123!"), Err(_)));
+    }
+
+    #[test]
+    fn test_parse_id_range() {
+        assert!(matches!(parse_id_range(b"1-2"), Ok(Range(1, 2))));
+        assert!(matches!(parse_id_range(b"12-99"), Ok(Range(12, 99))));
+        assert!(matches!(parse_id_range(b"12"), Err(_)));
+        assert!(matches!(parse_id_range(b"12-"), Err(_)));
+        assert!(matches!(parse_id_range(b"-12"), Err(_)));
+    }
+
+    #[test]
+    fn test_parse_ranges() -> anyhow::Result<()> {
+        assert_eq!(parse_input(b"")?, vec![]);
+        assert_eq!(parse_input(b"1-2")?, vec![Range(1, 2)]);
+        assert_eq!(parse_input(b"1-2,3-4")?, vec![Range(1, 2), Range(3, 4)]);
+        Ok(())
+    }
+
+    fn parse_input(input: &[u8]) -> anyhow::Result<Vec<Range>> {
+        let reader = Cursor::new(input);
+        ranges_from_input(reader)?.collect::<anyhow::Result<Vec<Range>>>()
+    }
 
     #[test]
     fn digits() {

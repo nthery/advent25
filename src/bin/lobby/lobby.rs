573db6307c865cbe7257cb52a74efe4e5a8a01d2
Nicolas Thery <nthery@gmail.com>
2026-01-01 16:50:29 +0100

    add empty day 3

---
+++ src/bin/lobby/lobby.rs
@@ -0,0 +1,14 @@
+// Day 3 - Lobby
+
+use std::io::{BufRead};
+
+fn main() -> anyhow::Result<()> {
+    let input = advent_code_25::open_input_file()?;
+    let answer = solve_for(input)?;
+    println!("answer: {}", answer);
+    Ok(())
+}
+
+fn solve_for<R: BufRead>(_input: R) -> anyhow::Result<usize> {
+    todo!()
+}

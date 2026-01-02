97be3294155a7265f713a2687e45c8539e833298
Nicolas Thery <nthery@gmail.com>
2026-01-02 15:17:37 +0100

    implement day 3 part 2

--- src/bin/lobby/lobby.rs
+++ src/bin/lobby/lobby.rs
@@ -5,6 +5,9 @@ use std::io::BufRead;
 mod iterator;
 use iterator::IteratorExt;
 
+// Number of batteries to turn on per bank.
+const NB_BATTERIES_ON: usize = 12;
+
 fn main() -> anyhow::Result<()> {
     let input = advent_code_25::open_input_file()?;
     let answer = solve_for(input)?;
@@ -29,30 +32,32 @@ fn solve_for<R: BufRead>(input: R) -> anyhow::Result<usize> {
 }
 
 fn max_joltage_for_bank(bank: &[u8]) -> usize {
-    let left_max_index = (&bank[0..bank.len() - 1])
-        .iter()
-        .first_max_position()
-        .unwrap();
-    let right_max_index = left_max_index
-        + 1
-        + (&bank[left_max_index + 1..])
+    let mut joltage = 0;
+
+    // Indices of window in bank where to search for next battery to turn on.
+    let mut i_first = 0;
+    let mut i_last = bank.len() - NB_BATTERIES_ON;
+
+    for _ in 0..NB_BATTERIES_ON {
+        let i = (bank[i_first..=i_last])
             .iter()
             .first_max_position()
             .unwrap();
-    joltage_for_selected_batteries(&bank, left_max_index, right_max_index)
+        joltage = joltage * 10 + from_digit(bank[i_first + i]);
+        i_first += i + 1;
+        i_last += 1;
+    }
+
+    joltage
 }
 
 /// Checks that `bank` contains only digits and is long enough.
 fn is_bank_valid(bank: &[u8]) -> bool {
-    bank.len() >= 2 && bank.iter().all(|b| b'0' <= *b && *b <= b'9')
-}
-
-fn joltage_for_selected_batteries(bank: &[u8], left_index: usize, right_index: usize) -> usize {
-    from_digit(bank[left_index]) * 10 + from_digit(bank[right_index])
+    bank.len() >= NB_BATTERIES_ON && bank.iter().all(|b| b'0' <= *b && *b <= b'9')
 }
 
 fn from_digit(b: u8) -> usize {
-    debug_assert!(b'0' <= b && b <= b'9');
+    debug_assert!(b.is_ascii_digit());
     (b - b'0') as usize
 }
 
@@ -65,23 +70,20 @@ mod tests {
         // too small
         assert!(!is_bank_valid(b""));
         assert!(!is_bank_valid(b"1"));
+        assert!(!is_bank_valid(b"12345678901"));
 
-        assert!(is_bank_valid(b"12"));
-        assert!(is_bank_valid(b"123"));
+        assert!(is_bank_valid(b"123456789012"));
+        assert!(is_bank_valid(b"1234567890123"));
 
         // non-digits
-        assert!(!is_bank_valid(b"1a"));
-    }
-
-    #[test]
-    fn test_joltage_for_selected_batteries() {
-        assert_eq!(joltage_for_selected_batteries(b"12", 0, 1), 12);
-        assert_eq!(joltage_for_selected_batteries(b"96", 0, 1), 96);
+        assert!(!is_bank_valid(b"123456789012@"));
     }
 
     #[test]
     fn test_max_joltage_for_bank() {
-        assert_eq!(max_joltage_for_bank(b"987654321111111"), 98);
-        assert_eq!(max_joltage_for_bank(b"811111111111119"), 89);
+        assert_eq!(max_joltage_for_bank(b"987654321111111"), 987654321111);
+        assert_eq!(max_joltage_for_bank(b"811111111111119"), 811111111119);
+        assert_eq!(max_joltage_for_bank(b"234234234234278"), 434234234278);
+        assert_eq!(max_joltage_for_bank(b"818181911112111"), 888911112111);
     }
 }

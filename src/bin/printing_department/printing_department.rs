e26dae0988251e25b7911b650d482e80b1f77114
Nicolas Thery <nthery@gmail.com>
2026-01-03 19:54:06 +0100

    day 4 part 2 passes

--- src/bin/printing_department/printing_department.rs
+++ src/bin/printing_department/printing_department.rs
@@ -14,16 +14,28 @@ fn main() -> anyhow::Result<()> {
 }
 
 fn solve_for<R: BufRead>(input: R) -> anyhow::Result<usize> {
-    let grid = Grid::load(input)?;
-    Ok(nb_accessible_rolls(&grid, false))
+    let mut grid = Grid::load(input)?;
+    let mut nb_removed_rolls = 0;
+    // Compute fixed point.
+    loop {
+        let n = remove_accessible_rolls(&mut grid, false);
+        if n == 0 {
+            break
+        }
+        nb_removed_rolls += n;
+    }
+    Ok(nb_removed_rolls)
 }
 
-fn nb_accessible_rolls(grid: &Grid, debug: bool) -> usize {
+/// Removes from `grid` all accessible rolls.
+/// Returns number of removed rolls.
+fn remove_accessible_rolls(grid: &mut Grid, debug: bool) -> usize {
     let mut sum = 0;
     for y in 0..grid.height() {
         for x in 0..grid.width() {
             if grid.is_roll_at(x, y) {
                 if nb_neighboring_rolls(grid, x, y) < 4 {
+                    grid.remove_roll_at(x, y);
                     sum += 1;
                     if debug {
                         print!("x");
@@ -73,22 +85,22 @@ mod tests {
 
     #[test]
     fn single_accessible_roll() {
-        let grid = new_grid(indoc! {br"
+        let mut grid = new_grid(indoc! {br"
             ...
             .@.
             ...
         "});
-        assert_eq!(nb_accessible_rolls(&grid, false), 1);
+        assert_eq!(remove_accessible_rolls(&mut grid, false), 1);
     }
 
     #[test]
     fn corners_are_acessible() {
-        let grid = new_grid(indoc! {br"
+        let mut grid = new_grid(indoc! {br"
             @.@
             .@.
             @.@
         "});
-        assert_eq!(nb_accessible_rolls(&grid, false), 4);
+        assert_eq!(remove_accessible_rolls(&mut grid, false), 5);
     }
 
     #[test]
